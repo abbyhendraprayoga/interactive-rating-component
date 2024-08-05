bf1c7f26bb582a0811bdf0604c0ef5ad5029211d
abbyhendraprayoga <voxartzyt@gmail.com>
2024-08-05 09:15:51 +0800

    fix(jscode)
    fixed ratings onclick bug

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
 const thankyouCard = document.querySelector(".thank-you-card");
 const ratingCard = document.querySelector(".rating-card");
 const ratings = document.querySelector(".ratings");
-const spans = ratings.querySelectorAll("span");
+const ratingValue = ratings.querySelectorAll("span");
 const submitBtn = document.querySelector(".submit-btn");
-const submit = document.querySelector(".submit");
+const submitText = document.querySelector(".submit");
 const loader = document.querySelector(".loader");
 
-spans.forEach((span) => {
-  span.addEventListener("click", () => {
-    spans.forEach((s) => s.classList.remove("selected"));
-    span.classList.add("selected");
+for (let i = 0; i < ratingValue.length; i++) {
+  ratingValue[i].addEventListener("click", () => {
+    for (let j = 0; j < ratingValue.length; j++) {
+      ratingValue[j].classList.remove("selected");
+    }
+    ratingValue[i].classList.add("selected");
   });
-});
+}
 
 submitBtn.addEventListener("click", () => {
-  const selectedSpan = [...spans].find((span) =>
-    span.classList.contains("selected")
-  );
-  if (selectedSpan) {
-    submit.classList.add("hidden");
+  let selectedRatings = [];
+  for (let i = 0; i < ratingValue.length; i++) {
+    if (ratingValue[i].classList.contains("selected")) {
+      selectedRatings.push(ratingValue[i]);
+      break;
+    }
+  }
+
+  if (selectedRatings) {
+    submitText.classList.add("hidden");
     loader.classList.remove("hidden");
     setTimeout(() => {
-      const selectedValue = selectedSpan.textContent;
-      thankyouCard.querySelector("#rating").textContent = selectedValue;
+      thankyouCard.querySelector("#rating").textContent =
+        selectedRatings[0].textContent;
       thankyouCard.classList.remove("hidden");
       ratingCard.classList.add("hidden");
-      setTimeout(() => submit.classList.remove("hidden"), 1500);
     }, 1500);
   } else {
     alert("Please select a rating");
